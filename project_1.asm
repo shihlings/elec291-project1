627cca858e13ec8cf4b14130351289ee0a41cc4b
Henry Rovner <rovnerhenry@gmail.com>
2024-02-06 10:34:22 -0800

    Added initial assembly code with temperature measurement

---
+++ project_1.asm
@@ -0,0 +1,360 @@
+; 76E003 ADC test program: Reads channel 7 on P1.1, pin 14
+; This version uses an LED as voltage reference connected to pin 6 (P1.7/AIN0)
+
+$NOLIST
+$MODN76E003
+$LIST
+
+;  N76E003 pinout:
+;                               -------
+;       PWM2/IC6/T0/AIN4/P0.5 -|1    20|- P0.4/AIN5/STADC/PWM3/IC3
+;               TXD/AIN3/P0.6 -|2    19|- P0.3/PWM5/IC5/AIN6
+;               RXD/AIN2/P0.7 -|3    18|- P0.2/ICPCK/OCDCK/RXD_1/[SCL]
+;                    RST/P2.0 -|4    17|- P0.1/PWM4/IC4/MISO
+;        INT0/OSCIN/AIN1/P3.0 -|5    16|- P0.0/PWM3/IC3/MOSI/T1
+;              INT1/AIN0/P1.7 -|6    15|- P1.0/PWM2/IC2/SPCLK
+;                         GND -|7    14|- P1.1/PWM1/IC1/AIN7/CLO
+;[SDA]/TXD_1/ICPDA/OCDDA/P1.6 -|8    13|- P1.2/PWM0/IC0
+;                         VDD -|9    12|- P1.3/SCL/[STADC]
+;            PWM5/IC7/SS/P1.5 -|10   11|- P1.4/SDA/FB/PWM1
+;                               -------
+;
+
+CLK               EQU 16600000 ; Microcontroller system frequency in Hz
+BAUD              EQU 115200 ; Baud rate of UART in bps
+TIMER1_RELOAD     EQU (0x100-(CLK/(16*BAUD)))
+TIMER0_RELOAD_1MS EQU (0x10000-(CLK/1000))
+
+ORG 0x0000
+	ljmp main
+
+;                     1234567890123456    <- This helps determine the location of the counter
+temp_message:     db 'Temperature:', 0
+cseg
+; These 'equ' must match the hardware wiring
+LCD_RS equ P1.3
+LCD_E  equ P1.4
+LCD_D4 equ P0.0
+LCD_D5 equ P0.1
+LCD_D6 equ P0.2
+	LCD_D7 equ P0.3
+MODE_BUTTON equ P1.5	
+
+$NOLIST
+$include(LCD_4bit.inc) ; A library of LCD related functions and utility macros
+$LIST
+
+; These register definitions needed by 'math32.inc'
+DSEG at 30H
+x:   ds 4
+y:   ds 4
+cold_junc_temp:	ds 4
+bcd: ds 5
+VREF: ds 2
+mode:	 ds 1
+
+BSEG
+mf: dbit 1
+
+$NOLIST
+$include(math32.inc)
+	$LIST
+
+Celsius_Unit_String:	db 0xDF, 'C', 0
+Celsius_Mode_String:	db 'Celsius\r\n', 0
+Kelvin_Unit_String:	db 'K ', 0
+Kelvin_Mode_String:	db 'Kelvin\r\n', 0
+Fahrenheit_Unit_String:	 db 0xDF, 'F', 0
+Fahrenheit_Mode_String:	 db 'Fahrenheit\r\n', 0
+	
+Init_All:
+	; Configure all the pins for bidirectional I/O
+	mov	P3M1, #0x00
+	mov	P3M2, #0x00
+	mov	P1M1, #0x00
+	mov	P1M2, #0x00
+	mov	P0M1, #0x00
+	mov	P0M2, #0x00
+	
+	orl	CKCON, #0x10 ; CLK is the input for timer 1
+	orl	PCON, #0x80 ; Bit SMOD=1, double baud rate
+	mov	SCON, #0x52
+	anl	T3CON, #0b11011111
+	anl	TMOD, #0x0F ; Clear the configuration bits for timer 1
+	orl	TMOD, #0x20 ; Timer 1 Mode 2
+	mov	TH1, #TIMER1_RELOAD ; TH1=TIMER1_RELOAD;
+	setb TR1
+	
+	; Using timer 0 for delay functions.  Initialize here:
+	clr	TR0 ; Stop timer 0
+	orl	CKCON,#0x08 ; CLK is the input for timer 0
+	anl	TMOD,#0xF0 ; Clear the configuration bits for timer 0
+	orl	TMOD,#0x01 ; Timer 0 in Mode 1: 16-bit timer
+	
+	; Initialize the pins used by the ADC (P1.1, P1.7) as input.
+	orl	P1M1, #0b10000010
+	anl	P1M2, #0b01111101
+	
+	; Initialize and start the ADC:
+	anl ADCCON0, #0xF0
+	orl ADCCON0, #0x07 ; Select channel 7
+	; AINDIDS select if some pins are analog inputs or digital I/O:
+	mov AINDIDS, #0x00 ; Disable all analog inputs
+	orl AINDIDS, #0b10000011 ; Activate AIN0 and AIN7 analog inputs
+	orl ADCCON1, #0x01 ; Enable ADC
+	
+	ret
+	
+wait_1ms:
+	clr	TR0 ; Stop timer 0
+	clr	TF0 ; Clear overflow flag
+	mov	TH0, #high(TIMER0_RELOAD_1MS)
+	mov	TL0,#low(TIMER0_RELOAD_1MS)
+	setb TR0
+	jnb	TF0, $ ; Wait for overflow
+	ret
+
+putchar:
+    jnb TI, putchar
+    clr TI
+    mov SBUF, a
+	ret
+
+; Send a constant-zero-terminated string using the serial port
+SendString:
+    clr A
+    movc A, @A+DPTR
+    jz SendStringDone
+    lcall putchar
+    inc DPTR
+    sjmp SendString
+SendStringDone:
+    ret
+
+put_x:
+	mov a, x+3
+	lcall putchar
+	mov a, x+2
+	lcall putchar
+	mov a, x+1
+	lcall putchar
+	mov a, x+0
+	lcall putchar
+	mov a, #'\r'
+	lcall putchar
+	mov a, #'\n'
+	lcall putchar
+	ret
+
+; Wait the number of miliseconds in R2
+waitms:
+	lcall wait_1ms
+	djnz R2, waitms
+	ret
+
+display_units:
+	mov a, mode
+	jnz display_units_b
+	Send_Constant_String(#Kelvin_Unit_String)
+	sjmp display_units_ret
+display_units_b:
+	xrl a, #0x01
+	jnz display_units_c
+	Send_Constant_String(#Celsius_Unit_String)
+	sjmp display_units_ret
+display_units_c:
+	Send_Constant_String(#Fahrenheit_Unit_String)
+display_units_ret:
+	ret
+	
+; We can display a number any way we want.  In this case with
+; four decimal places.
+Display_formated_BCD:
+	Set_Cursor(2, 1)
+	Display_BCD(bcd+2)
+	Display_BCD(bcd+1)
+	Display_char(#'.')
+	Display_BCD(bcd+0)
+	lcall display_units
+	ret
+
+Read_ADC:
+	clr ADCF
+	setb ADCS ;  ADC start trigger signal
+    jnb ADCF, $ ; Wait for conversion complete
+    
+    ; Read the ADC result and store in [R1, R0]
+    mov a, ADCRL
+    anl a, #0x0f
+    mov R0, a
+    mov a, ADCRH   
+    swap a
+    push acc
+    anl a, #0x0f
+    mov R1, a
+    pop acc
+    anl a, #0xf0
+    orl a, R0
+    mov R0, A
+	ret
+
+send_mode:
+	jnz send_mode_b
+	mov DPTR, #Kelvin_Mode_String
+	lcall SendString
+	sjmp send_mode_ret
+send_mode_b:
+	xrl a, #0x01
+	jnz send_mode_c
+	mov DPTR, #Celsius_Mode_String
+	lcall SendString
+	sjmp send_mode_ret
+send_mode_c:
+	mov DPTR, #Fahrenheit_Mode_String
+	lcall SendString
+send_mode_ret:
+	ljmp cycle_mode_ret_b
+	
+cycle_mode:
+	mov a, mode
+	add a, #0x01
+	cjne a, #0x03, cycle_mode_ret
+	clr a
+cycle_mode_ret:
+	mov mode, a
+	ljmp send_mode
+cycle_mode_ret_b:	
+	ret
+
+convert_temp:
+	mov a, mode
+	jnz convert_temp_b
+	ljmp convert_temp_ret
+convert_temp_b:
+	load_y(27315) 	
+	lcall sub32
+	xrl a, #0x01
+	jnz convert_temp_c
+	ljmp convert_temp_ret
+convert_temp_c:
+	load_y(18)
+	lcall mul32
+	load_y(10)
+	lcall div32
+	load_y(3200)
+	lcall add32
+convert_temp_ret:
+	ret
+
+poll_for_mode_change:
+	jnb RI, poll_for_mode_change_ret
+	clr RI
+	lcall cycle_mode
+poll_for_mode_change_ret:
+	ret
+	
+main:
+	mov sp, #0x7f
+	lcall Init_All
+	lcall LCD_4BIT
+
+	mov mode, #0x01
+    
+    ; initial messages in LCD
+	Set_Cursor(1, 1)
+    Send_Constant_String(#temp_message)
+	Set_Cursor(2, 1)
+    
+Forever:
+
+	jb MODE_BUTTON, Forever_b
+	Wait_Milli_Seconds(#10)
+	jb MODE_BUTTON, Forever_b
+	jnb MODE_BUTTON, $
+	lcall cycle_mode
+
+Forever_b:
+	lcall poll_for_mode_change
+	; Read the 2.08V LED voltage connected to AIN0 on pin 6
+	anl ADCCON0, #0xF0
+	orl ADCCON0, #0x00 ; Select channel 0
+
+	lcall Read_ADC
+	; Save result for later use
+	mov VREF+0, R0
+	mov VREF+1, R1
+
+	; Read the signal connected to AIN7
+	anl ADCCON0, #0xF0
+	orl ADCCON0, #0x07 ; Select channel 7
+	lcall Read_ADC
+    
+    ; Convert to voltage
+	mov x+0, R0
+	mov x+1, R1
+	; Pad other bits with zero
+	mov x+2, #0
+	mov x+3, #0
+	Load_y(41132) ; The MEASURED LED voltage: 2.074V, with 4 decimal places
+	lcall mul32
+	; Retrive the ADC LED value
+	mov y+0, VREF+0
+	mov y+1, VREF+1
+	; Pad other bits with zero
+	mov y+2, #0
+	mov y+3, #0
+	lcall div32
+
+	load_y(800) 		; Adjustment based on thermostat readings
+	lcall add32
+
+	lcall convert_temp
+
+	mov cold_junc_temp+0, x+0
+	mov cold_junc_temp+1, x+1
+	mov cold_junc_temp+2, x+2
+	mov cold_junc_temp+3, x+3
+
+	anl ADCCON0, #0xF0
+	orl ADCCON0, #0x01 ; Select channel 1
+	lcall Read_ADC
+
+	mov x+0, R0
+	mov x+1, R1
+	mov x+2, #0
+	mov x+3, #0
+
+	Load_y(41132)
+	lcall mul32
+	mov y+0, VREF+0
+	mov y+1, VREF+1
+	mov y+2, #0
+	mov y+3, #0
+	lcall div32
+
+	load_y(7391) 		; Amplifier gain means 73.91 degrees C / Volt
+	lcall mul32
+	load_y(10000)
+	lcall div32
+
+	mov y+0, cold_junc_temp+0
+	mov y+1, cold_junc_temp+1
+	mov y+2, cold_junc_temp+2
+	mov y+3, cold_junc_temp+3
+	lcall add32
+
+	; Convert to BCD and display
+	lcall hex2bcd
+	lcall Display_formated_BCD
+
+	lcall put_x
+	
+	; Wait 500 ms between conversions
+	mov R2, #250
+	lcall waitms
+	mov R2, #250
+	lcall waitms
+	
+	ljmp Forever
+END
+	
